304854efaeee26e89a096004140144e0164491e7
Dorian Dobler <dorian.dobler03@htl-salzburg.ac.at>
2024-01-13 20:34:18 +0100

    grüße an dich melli, plz ist auch mit drinnen

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const db = require('./db'); // Verbinde dich zur Datenbank
+const bcrypt = require('bcrypt');
+const userRoutes = require('./Routes/userRoutes'); // Anpassung hier
+const jobRoutes = require('./Routes/jobRoutes');
+const eventRoutes = require('./Routes/eventRoutes'); // Anpassung hier
+const loanRoutes = require('./Routes/loanRoutes');
+
+app.use(express.json());
+
+// Verbinde Routen
+app.use('/users', userRoutes);
+app.use('/jobs', jobRoutes);
+app.use('/event', eventRoutes);
+app.use('/loan', loanRoutes);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
