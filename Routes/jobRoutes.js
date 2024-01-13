4ecc118ae6d7d204d6c84d5154a9868d1ce46092
Dorian Dobler <dorian.dobler03@htl-salzburg.ac.at>
2024-01-13 23:30:40 +0100

    annahme schleife job

--- Routes/jobRoutes.js
+++ Routes/jobRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/jobController');
 
-router.get('/', jobController.getAllJobs);
+router.get('/all', jobController.getAllJobs);
+router.post('/me', jobController.getJobsByPLZ);
+router.post('/me/apply/:jobID', jobController.applyForJob);
 router.post('/create', jobController.createJob);
 router.post('/accept', jobController.acceptJob);
 router.get('/archive', jobController.getArchivedJobs);
